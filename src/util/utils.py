61a45d6f9f6286c300ac46ddc63d6e8520d2e8b9
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2022-05-28 17:55:25 +0530

    delete all the files

--- src/util/utils.py
+++
@@ -1,7 +0,0 @@
-""" Common utility functions """
-
-
-def snake_case(string_to_convert):
-    """ Returns snake_case representation of CamelCase string """
-    return ''.join(['_'+i.lower() if i.isupper()
-                    else i for i in string_to_convert]).lstrip('_')
